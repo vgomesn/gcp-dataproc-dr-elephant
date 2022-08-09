ae86fbb74c9dc07a00faf28877824112d98f5b78
Veronica Gomes <vgomesn@gmail.com>
2022-08-09 15:06:07 +0000

    dr-elephant dataproc added env variables & drop db

---
+++ set-env.sh
@@ -0,0 +1,7 @@
+export HADOOP_HOME=/usr/lib/hadoop
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+export PATH=$HADOOP_HOME/bin:$PATH
+export SPARK_HOME=/usr/lib/spark
+export SPARK_CONF_DIR=/etc/spark/conf
+export PATH=$HADOOP_HOME/bin:$SPARK_HOME/bin:$PATH
+
